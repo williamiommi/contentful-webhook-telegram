233cc22e9d6b5cd769470b47595b98bc8fc0b1ff
williamiommi <william.iommi@gmail.com>
2023-12-03 21:31:16 +0100

    working on sending message to channel

--- api/telegram/message.js
+++ api/telegram/message.js
@@ -1,3 +1,5 @@
+const { Bot } = require("grammy");
+
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
@@ -8,11 +10,54 @@ const allowCors = (fn) => async (req, res) => {
   return await fn(req, res);
 };
 
-const handler = (request, response) => {
+const handler = async (request, response) => {
   try {
-    console.log("received call from telegram sender");
-    console.log(request.headers);
-    console.log(request.body);
+    // verify contentful signature
+    // https://www.contentful.com/developers/docs/extensibility/app-framework/request-verification/#verifying-requests-on-the-backend
+    // TODO
+
+    // verify body parameters
+    const {
+      media,
+      textMessage,
+      linkUrl,
+      linkName,
+      textMessageParseMode,
+      channelName,
+    } = request.body;
+
+    if ((!media && !textMessage) || !channelName) {
+      console.error("Missing required parameters");
+      throw new Error("Missing required parameters");
+    }
+
+    // create bot
+    const telegramBot = new Bot(process.env.BOT_TOKEN);
+
+    // create link if available
+    let inlineLink;
+    if (linkUrl) {
+      inlineLink = new InlineKeyboard().url(linkName || "Read more", linkUrl);
+    }
+
+    // understand what type of message you need to send
+    if (media) {
+      // sending media type message (w/ optional link). Media can be video or simple image. We use two different methods
+      const mediaMethod = media.endsWith(".mp4") ? "sendVideo" : "sendPhoto";
+
+      await telegramBot.api[mediaMethod](channelName, media, {
+        parse_mode: textMessageParseMode,
+        ...(textMessage && { caption: textMessage }),
+        ...(inlineLink && { reply_markup: inlineLink }),
+      });
+    } else {
+      // sending simple text message (w/ optional link)
+      await telegramBot.api.sendMessage(channelName, textMessage, {
+        parse_mode: textMessageParseMode,
+        ...(inlineLink && { reply_markup: inlineLink }),
+      });
+    }
+
     response.status(200).json({ status: "ok" });
   } catch (e) {
     console.error(e);
